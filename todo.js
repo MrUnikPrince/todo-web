9591fb72926f25144ffce2311945d22bb29af725
MrUnikPrince <opuniqueprince@gmail.com>
2023-07-12 01:29:01 +0530

    fixed some bugs

--- todo.js
+++ todo.js
@@ -3,12 +3,10 @@ const taskList = document.getElementById('list');
 const addTaskInput = document.getElementById('add');
 const tasksCounter = document.getElementById('tasks-counter');
 
-console.log('Working');
 
 // Add to dom function 
 function addTaskToDom(task){
     const li = document.createElement('li');
-
     li.innerHTML = `
     <input type="checkbox" id="${task.id}" ${task.done ? "checked" : ''} class="custom-checkbox">
     <label for="${task.id}">${task.text}</label>
@@ -19,10 +17,7 @@ function addTaskToDom(task){
 
 // render Function
 function renderList() {
-
-    
     taskList.innerHTML = "";
-
     for(let i = 0; i < tasks.length; i++){
         addTaskToDom(tasks[i]);
     }
@@ -32,7 +27,7 @@ function renderList() {
 // Task Complete Task Function
 function markTaskAsComplete(taskId) {
     const task = tasks.filter(function (task) {
-        return task.Id === taskId
+        return task.id === taskId
     })
 
     if (task.length > 0) {
@@ -42,8 +37,10 @@ function markTaskAsComplete(taskId) {
         showNotification("Task toggled successfully");
         return;
     }
-
     showNotification("Could not toggle the task");
+
+
+    // showNotification("Could not toggle the task");
 
 }
 
