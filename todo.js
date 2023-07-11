eab73fe0adf64bd712ab92ea21cb0c8c1bbaee0c
MrUnikPrince <opuniqueprince@gmail.com>
2023-07-11 16:26:07 +0530

    Getting User Input and also completing  showNotification  function

--- todo.js
+++ todo.js
@@ -5,12 +5,40 @@ const tasksCounter = document.getElementById('tasks-counter');
 
 console.log('Working');
 
+
+// render Function
 function renderList () {}
 
+// Task Complete Task Function
 function markTaskAsComplete (taskId) {}
 
+// Delete Task function
 function deleteTask (taskId) {}
 
+// Add task Funtion
 function addTask (task) {}
 
-function showNotification(text) {}
+// Show Notification Function
+function showNotification(text) {
+    alert(text);
+}
+
+function handleInputKeyPress(e){
+    if(e.key === 'Enter'){
+        const text = e.target.value;
+    }    
+    if(!text){
+        showNotification("Task can not be empty");
+        return;
+    }
+
+    const task = {
+        text,
+        id: Date.now().toString(),
+        done: false
+    }
+
+    e.target.value = "";
+    addTask(task);
+}
+addTaskInput.addEventListener('keyup', handleInputKeyPress);
